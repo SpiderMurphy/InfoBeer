e561b616628832ba15c6295589d69847d71c3d85
SpiderMurphy <vincenzocaleca@gmail.com>
2018-03-12 10:12:43 +0100

    Final Release

--- app/src/main/java/com/hero/infobeer/MainActivity.kt
+++ app/src/main/java/com/hero/infobeer/MainActivity.kt
@@ -2,6 +2,7 @@ package com.hero.infobeer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
@@ -9,6 +10,7 @@ import android.support.v7.widget.SearchView
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import com.android.volley.Response
 import com.hero.infobeer.adapters.ItemAdapter
 import com.hero.infobeer.models.Brewery
@@ -27,6 +29,23 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(wx_toolbar)
         wx_rw_beers.layoutManager = LinearLayoutManager(this)
         wx_rw_beers.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+
+        btn_sort.setOnClickListener { view -> if(this@MainActivity.wx_rw_beers.adapter != null){
+            var adapter = wx_rw_beers.adapter as ItemAdapter
+
+            if(btn_sort.tag.equals("asc")) {
+                adapter.presenter.sort(true)
+                btn_sort.tag = "desc"
+                btn_sort.setCompoundDrawables(null, null, ContextCompat.getDrawable(this@MainActivity, R.drawable.ic_arrow_upward_black_24dp), null)
+            }
+            else{
+                adapter.presenter.sort()
+                btn_sort.tag = "asc"
+                btn_sort.setCompoundDrawables(null, null, ContextCompat.getDrawable(this@MainActivity, R.drawable.ic_arrow_downward_black_24dp), null)
+            }
+
+            wx_rw_beers.adapter.notifyDataSetChanged()
+        } }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -50,6 +69,11 @@ class MainActivity : AppCompatActivity() {
                         Brewery::class.java,
                         null,
                         Response.Listener { response ->
+                            if(response.beers.size > 0)
+                                wx_rw_beers.visibility = View.VISIBLE
+                            else
+                                wx_rw_beers.visibility = View.GONE
+
                             var presenter = ItemPresenter()
 
                             response.beers.forEach {
--- app/src/main/java/com/hero/infobeer/presenters/ItemPresenter.kt
+++ app/src/main/java/com/hero/infobeer/presenters/ItemPresenter.kt
@@ -2,6 +2,7 @@ package com.hero.infobeer.presenters
 
 import com.hero.infobeer.adapters.PresenterBehaviors
 import com.hero.infobeer.models.ItemView
+import java.util.*
 
 /**
  * Created by Cyan on 12/03/2018.
@@ -14,5 +15,14 @@ class ItemPresenter {
 
     fun bindView(holder: PresenterBehaviors, position : Int){
         holder.bindItem(items.get(position))
+    }
+
+    fun sort(asc: Boolean = false) {
+        if(!asc)
+            Collections.sort(items, kotlin.Comparator { t1, t -> if (t1.item.title.compareTo(t.item.title) > 0)  -1
+            else ( if(t1.item.title.compareTo(t.item.title) < 0) 1 else 0)  })
+        else
+            Collections.sort(items, kotlin.Comparator { t1, t -> if (t1.item.title.compareTo(t.item.title) > 0) 1
+            else ( if(t1.item.title.compareTo(t.item.title) < 0) -1 else 0)  })
     }
 }
